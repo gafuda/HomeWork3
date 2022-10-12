22bb120e1ecd42fd1df92c563711ad4ff282bcc0
Mariia <marii.ch@bk.ru>
2022-10-12 23:38:03 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// // Задача 1
+﻿// Задача 1
 // Console.WriteLine("Введите первое число:");
 // 	int firstnumb = int.Parse(Console.ReadLine());
 // 	Console.WriteLine("Введите второе число:");
@@ -7,23 +7,33 @@
 //         for (i = firstnumb; i <= secondnumb; i++)
 //             Console.Write(i*i*i + " ");
 
-
+// Задача 2
 // Console.WriteLine("Введите число:");
 // int num = int.Parse(Console.ReadLine());
 //     int a = 0;
 //     while (num > 0)
 //     {
- 
 //         a = a + num % 10;
 //         num = num /10 ;
- 
 //     }
 //  Console.WriteLine(a);
 
-Console.WriteLine("Введите число:");
-int num = int.Parse(Console.ReadLine());
-int i;
-int a = 1;
-for (i = 1; i <= num; i++);
-    a = i * a;
-    Console.Write(a);
+// Задача 3
+// Console.WriteLine("Введите число:");
+//     int num = int.Parse(Console.ReadLine());
+//     int a = 1;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         a = i * a;
+//     }
+//     Console.Write(a);
+
+// Задача 4
+// int[] numbers = new int[] {22,234,6457,33,435,88, 64};
+ // for (int i = 0; i < numbers.Length; i++)
+// {
+//      int number = numbers[i];
+//      int cube = (int)Math.Pow(number, 3);
+//      if (cube % 2 == 0);
+//      Console.WriteLine("Куб числа " + number + " равен " + cube);
+// }
